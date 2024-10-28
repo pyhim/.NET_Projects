85effc71c5765408779e46267c4b82e9e9fa45cb
dgalytskyi <galitskiy.dima8@gmail.com>
2024-10-28 19:28:22 +0200

    new lesson project

---
+++ ExceptionHandling/Calculator.cs
@@ -0,0 +1,11 @@
+namespace ExceptionHandling;
+
+public class Calculator
+{
+    public static void Run()
+    {
+        Console.Write("Please enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Please enter a desirable");
+    }
+}

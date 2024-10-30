77d4306c37ca7cd42c77c8c5f17b061d6163eac0
dgalytskyi <galitskiy.dima8@gmail.com>
2024-10-30 15:39:52 +0200

    new tests

---
+++ ExceptionHandling/Run.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace ExceptionHandling;
+
+class Run
+{
+    static void Main(string[] args)
+    {
+        var a = BaseNumberConverter
+        
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ OperatorOverloading1/Run.cs
@@ -0,0 +1,26 @@
+﻿namespace OperatorOverloading1;
+
+class Run
+{
+    public static void Main(string[] args)
+    {
+        // var matrix1 = new MathematicalMatrix(4, 4);
+        // var matrix2 = new MathematicalMatrix(4, 4);
+        // matrix1.Print();
+        // matrix2.Print();
+        //
+        // var matrix3 = matrix1 + matrix2;
+        // matrix3.Print();
+        // matrix3 = matrix1 - matrix2;
+        // matrix3.Print();
+        // matrix3 = matrix1 * matrix2;
+        // matrix3.Print();
+        // matrix3 = matrix1 * 2;
+        // matrix3.Print();
+
+        var books = new BookList();
+        books.Print();
+        books.Remove(3);
+        books.Print();
+    }
+}

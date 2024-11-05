674e802251c7bc2c6e3255bf2a964f147e794f96
dgalytskyi <galitskiy.dima8@gmail.com>
2024-11-05 23:51:51 +0200

    adjusted to meet requirements

--- ExceptionHandling/Calculator.cs
+++ ExceptionHandling/Calculator.cs
@@ -5,10 +5,17 @@ public class Calculator
     public Calculator()
     {
         Console.Write("Please enter a number: ");
-        var number = Convert.ToInt32(Console.ReadLine());
+        var input = Convert.ToInt64(Console.ReadLine());
+        
+        if (input > int.MaxValue)
+        {
+            throw new ArgumentException("The input is too large to fit the calculator");
+        }
+        var number = (int)input;
+        
         Console.Write("Please enter a desirable base conversion: ");
         var toBase = Convert.ToInt32(Console.ReadLine());
-
+        
         var result = Convert.ToString(number, toBase);
         Console.WriteLine($"{number} in {toBase} base is: {result}");
     }
--- ExceptionHandling/Run.cs
+++ ExceptionHandling/Run.cs
@@ -4,9 +4,9 @@ public class Run
 {
     public static void Main(string[] args)
     {
-        // var calc = new Calculator();
+        var calc = new Calculator();
 
-        var foreignPassport1 = new ForeignPassport("31242143", "John", "Smith", "USA", DateTime.Parse("10/14/2006"));
-        var foreignPassport2 = new ForeignPassport("31242143", "Joh+-n", "Smit3h", "USA", DateTime.Parse("10/14/2006"));
+        // var foreignPassport1 = new ForeignPassport("31242143", "John", "Smith", "USA", DateTime.Parse("10/14/2006"));
+        // var foreignPassport2 = new ForeignPassport("31242143", "Joh+-n", "Smit3h", "USA", DateTime.Parse("10/14/2006"));
     }
 }

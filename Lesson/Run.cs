08a7ab201c32800dd7dd2541622d3f5e3f6bd408
dgalytskyi <galitskiy.dima8@gmail.com>
2024-11-11 18:50:06 +0200

    release

---
+++ Lesson/Run.cs
@@ -0,0 +1,17 @@
+﻿using Lesson.ArrayIOutput;
+
+namespace Lesson;
+
+internal class Run
+{
+    public static void Main(string[] args)
+    {
+        var f1 = new Fraction(6, 5);
+        var f2 = new Fraction(4, 5);
+        
+        Console.WriteLine(f1 + f2);
+        Console.WriteLine(f1 - f2);
+        Console.WriteLine(f1 * f2);
+        Console.WriteLine(f1 / f2);
+    }
+}

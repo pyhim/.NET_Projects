e115fc1664cc719b2251cb29b8a66a51c4880872
dgalytskyi <galitskiy.dima8@gmail.com>
2024-11-13 18:42:14 +0200

    release

---
+++ Lesson/Delegates/ArithmeticApp.cs
@@ -0,0 +1,62 @@
+namespace Lesson.Delegates;
+
+public class ArithmeticApp
+{
+    delegate bool CheckNumber(int number);
+    
+    public ArithmeticApp()
+    {
+        CheckNumber[] checkNumber = [IsEven, IsOdd, IsPrime, IsFibonacci];
+        string[] names = ["is even", "is odd", "is prime", "is fibonacci"];
+        
+        Console.Write("Enter the number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        foreach ((CheckNumber checker, string name) in checkNumber.Zip(names, (x, y) => (x, y)))
+        {
+            if (checker(number))
+            {
+                Console.WriteLine($"The number {name}");
+            }
+        }
+    }
+
+    private static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+    private static bool IsOdd(int number)
+    {
+        return number % 2 != 0;
+    }
+
+    private static bool IsPrime(int number)
+    {
+        switch (number)
+        {
+            case <= 1:
+                return false;
+            case 2:
+                return true;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0) return false;
+        }
+        
+        return true;
+    }
+
+    private static bool IsPerfectSquare(int number)
+    {
+        int sqrt = (int)Math.Sqrt(number);
+        return sqrt * sqrt == number;
+    }
+
+    private static bool IsFibonacci(int number)
+    {
+        return IsPerfectSquare(5 * number * number + 4) || IsPerfectSquare(5 * number * number - 4);
+    }
+}
--- Lesson/Run.cs
+++ Lesson/Run.cs
@@ -1,4 +1,4 @@
-﻿using Lesson.ArrayIOutput;
+﻿using Lesson.Delegates;
 
 namespace Lesson;
 
@@ -6,12 +6,6 @@ internal class Run
 {
     public static void Main(string[] args)
     {
-        var f1 = new Fraction(6, 5);
-        var f2 = new Fraction(4, 5);
-        
-        Console.WriteLine(f1 + f2);
-        Console.WriteLine(f1 - f2);
-        Console.WriteLine(f1 * f2);
-        Console.WriteLine(f1 / f2);
+        var app = new ArithmeticApp();
     }
 }

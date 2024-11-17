ba2b4221c6080e76ac4b73b60dd069cc893fb089
dgalytskyi <galitskiy.dima8@gmail.com>
2024-11-17 10:39:21 +0100

    release

--- Lesson/Run.cs
+++ Lesson/Run.cs
@@ -1,4 +1,4 @@
-﻿using Lesson.Delegates;
+﻿using Lesson.ArrayIOutput;
 
 namespace Lesson;
 
@@ -6,6 +6,13 @@ internal class Run
 {
     public static void Main(string[] args)
     {
-        var app = new ArithmeticApp();
+        var array = new MyArray([1, 2, 6, 6, 12, 5, 7, 1, 90, 3]);
+
+        Console.WriteLine(array.Less(5));
+        Console.WriteLine(array.Greater(5));
+        array.ShowEven();
+        array.ShowOdd();
+        Console.WriteLine(array.CountDistinct());
+        Console.WriteLine(array.EqualToValue(1));
     }
 }

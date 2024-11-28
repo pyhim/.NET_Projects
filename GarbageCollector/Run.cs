25c5e8f087037589fe701e6a22411b72b8f480d0
dgalytskyi <galitskiy.dima8@gmail.com>
2024-11-28 09:11:50 +0100

    release

---
+++ GarbageCollector/Run.cs
@@ -0,0 +1,13 @@
+﻿namespace GarbageCollector;
+
+internal class Run
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < 1000000; i++)
+        {
+            var temp1 = new TheatrePlay("sdfsdfsaf", "bfdfgs", "fq3rdf", "jkg782", DateTime.Parse("10/01/2021"));
+            var temp2 = new Shop("someshop", ShopType.Shoe, "s,jdhfjkhsd");
+        }
+    }
+}
